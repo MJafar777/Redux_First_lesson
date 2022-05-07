3b83209d44145410c1847925ca40d33ada65f6ae
jafar <mirzaraximovjafar2001@gmail.com>
2022-05-07 10:06:02 +0500

    1

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,7 +3,33 @@ import React from "react";
 import { connect } from "react-redux";
 const SongList = (props) => {
   console.log(props);
-  return <div></div>;
+  const renderList = () => {
+    return props.songList.map((val) => {
+      return (
+        <div className="card" key={val.name}>
+          <div className="content">
+            <img
+              className="right floated mini ui image"
+              src="https://play-lh.googleusercontent.com/QovZ-E3Uxm4EvjacN-Cv1LnjEv-x5SqFFB5BbhGIwXI_KorjFhEHahRZcXFC6P40Xg"
+            />
+            <div className="header">{val.name}</div>
+            <div className="meta">{val.time}</div>
+            <div className="description">{val.author}</div>
+          </div>
+          <div className="extra content">
+            <div className="ui two buttons">
+              <div className="ui basic red button">Select Song</div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+  return (
+    <>
+      <div className="ui cards">{renderList()}</div>
+    </>
+  );
 };
 
 const getMyState = (state) => {
